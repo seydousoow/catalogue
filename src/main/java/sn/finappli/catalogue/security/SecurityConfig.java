f731de9035ddf154602b68a372f472c328cdf3fc
seydousoow <seydousoow@gmail.com>
2020-06-10 14:16:57 +0000

    remove jetbrains annotations

--- src/main/java/sn/finappli/catalogue/security/SecurityConfig.java
+++ src/main/java/sn/finappli/catalogue/security/SecurityConfig.java
@@ -1,6 +1,5 @@
 package sn.finappli.catalogue.security;
 
-import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -21,7 +20,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(@NotNull AuthenticationManagerBuilder auth) throws Exception {
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
         auth.inMemoryAuthentication()
                 .passwordEncoder(encoder())
                 .withUser("test")
@@ -30,7 +29,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(@NotNull HttpSecurity http) throws Exception {
+    protected void configure(HttpSecurity http) throws Exception {
         http
                 .csrf().disable()
                 .sessionManagement()
